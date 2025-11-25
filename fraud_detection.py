fed4a3e72968c5dceec35f473fa8220f222efe7f
mmiskatul <masabimiskat@gmail.com>
2025-11-25 23:01:30 +0600

    add some section on the streamlit

--- fraud_detection.py
+++ fraud_detection.py
@@ -10,3 +10,12 @@ st.markdown("Please Enter the transaction details and use the predict button")
 
 st.divider() 
 
+
+transaction_type = st.selectbox("Transaction Type",["PAYMENT","TRANSFER","CASH_OUT","DEPOSIT"]) 
+amount = st.number_input("Amount",min_value =0.0, value= 1000.0) 
+oldbalanceOrg = st.number_input("Old Balance (Sender)",min_value=0.0,value=10000.0) 
+newbalanceOrig = st.number_input("New Balance (Sender)",min_value=0.0,value=9000.0) 
+oldbalanceDest =st.number_input("Old Balance (Receiver)",min_value=0.0,value=0.0)
+newbalanceDest =st.number_input("New Balance (Receiver)",min_value=0.0,value=0.0) 
+
+
